fc25eac34c426a2aca577dbc24917eea4ce59b5a
Adriel Josias <adrielj47@gmail.com>
2022-01-05 21:35:21 -0300

    Projeto criado

---
+++ src/main/java/com/adrieljosias/cursomc/CusomcApplication.java
@@ -0,0 +1,13 @@
+package com.adrieljosias.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CusomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CusomcApplication.class, args);
+	}
+
+}
